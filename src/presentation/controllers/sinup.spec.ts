dbf1efc2440377b9647db583fda5d782b7769ffb
wellhenrique <welissonh80@gmail.com>
2022-07-20 23:43:08 -0300

    refactor: emailMockSut

--- src/presentation/controllers/sinup.spec.ts
+++ src/presentation/controllers/sinup.spec.ts
@@ -1,7 +1,15 @@
 import { SignupController } from "./signup";
 import { MissingParamError } from "../erros/missing-param-error";
+import { InvalidParamError } from "../erros/invalid-param-error";
 
 const makeSut = (): SignupController => {
+  class EmailValidator {
+    isValid(email: String): boolean {
+      return true;
+    }
+  }
+
+  const emailValidator = new EmailValidator();
   return new SignupController();
 };
 
@@ -66,5 +74,21 @@ describe("Signup Controller", () => {
     expect(httpResponse.body).toEqual(
       new MissingParamError("passwordConfirmation")
     );
+  });
+
+  it("Should return 400 if an invalid email is provided", () => {
+    const sut = makeSut();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@email.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError("Email"));
   });
 });
